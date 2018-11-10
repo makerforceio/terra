f2ff7ab6b48fd77b46bf1b08fe31241e38b8d835
Sudharshan <sudhar393@gmail.com>
2018-11-10 15:11:25 +0800

    overview scaffold

---
+++ src/components/PlantInfo.vue
@@ -0,0 +1,13 @@
+<template>
+	<h1>Hi</h1>
+</template>
+
+<script>
+export default {
+	name: 'PlantInfo',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,3 +1,23 @@
 <template>
-	<span>meh</span>
+	<div class="container">
+	</div>
 </template>
+
+<script>
+export default {
+	name: 'Overview',
+	components: {
+	},
+	data() {
+		return {
+			plantlist: [
+				{ name: 'Cucumber', health: 50, image: 'aseDf' },
+				{ name: 'Tomato', health: 35, image: 'dfWgF' },
+			],
+		};
+	},
+};
+</script>
+
+<style scoped>
+</style>
