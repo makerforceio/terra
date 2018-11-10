db10a5b75036a698bd5efd4dee78e1bc144a2840
Sudharshan <sudhar393@gmail.com>
2018-11-10 16:34:21 +0800

    rough ui for plants

--- src/components/PlantInfo.vue
+++ src/components/PlantInfo.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="infobox">
-		<img :src="plant.img" class="character">
+		<img :src="plant.image" class="character">
+		<h3 class="name">{{ plant.name }}</h3>
+		<h4 class="lvl">Lvl {{ plant.level }}</h4>
+		<progress max="100" :value="plant.health"></progress>
 	</div>
 </template>
 
@@ -12,10 +15,50 @@ export default {
 			image: String,
 			name: String,
 			health: Number,
+			level: Number,
 		},
 	},
 };
 </script>
 
 <style scoped>
+.infobox {
+	width: 40vw;
+	height: 40vw;
+	padding: 10px;
+}
+
+.img {
+	width: 100px;
+}
+
+.lvl, .name {
+	display: inline-block;
+	margin: 0px;
+}
+
+.lvl {
+	float: right;
+}
+
+progress[value] {
+  /* Reset the default appearance */
+  -webkit-appearance: none;
+  appearance: none;
+
+  width: 100%;
+  height: 10px;
+}
+
+progress[value]::-webkit-progress-bar {
+  background-color: #eee;
+  border-radius: 2px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
+}
+
+progress[value]::-webkit-progress-value {
+  background-color: green;
+		border-radius: 2px;
+		background-size: 35px 20px, 100% 100%, 100% 100%;
+}
 </style>
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -3,7 +3,9 @@
 		<Status class="status"
 			:playerName="playerName"
 			:score="score" />
-		<PlantInfo v-for="plant in plants" :plant="plant" :key="plant.id"/>
+		<div class="plantlist">
+			<PlantInfo v-for="plant in plants" :plant="plant" :key="plant.id"/>
+		</div>
 	</div>
 </template>
 
@@ -28,12 +30,14 @@ export default {
 				{
 					name: 'Cucumber',
 					health: 50,
-					image: '../assets',
+					image: '../assets/logo.png',
+					level: 20,
 					id: 0,
 				},
 				{
 					name: 'Tomato',
 					health: 35,
+					level: 30,
 					image: 'dfWgF',
 					id: 1,
 				},
@@ -44,5 +48,11 @@ export default {
 </script>
 
 <style scoped>
-
+.plantlist {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: space-evenly;
+	align-content: flex-start;
+}
 </style>
