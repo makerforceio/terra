c84bf24bb574e5c118d796969cdeaacf2b111f83
Ambrose Chua <ambrose@makerforce.io>
2018-11-10 14:31:50 +0800

    Base

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div class="app">
+		<nav>
+		<ul>
+		<router-link tag="li" to="/">Dashboard</router-link>
+		<router-link tag="li" to="/map">Map</router-link>
+		</ul>
+		</nav>
+		<router-view/>
+	</div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+<style scoped>
+
+.app {
+	width: 100%;
+	height: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/Overview.vue
@@ -0,0 +1,3 @@
+<template>
+	<span>meh</span>
+</template>
