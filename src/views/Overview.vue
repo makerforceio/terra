f07104cb0014a3db8787a56352c775f88a7fdecc
Ambrose Chua <ambrose@makerforce.io>
2018-11-10 15:52:34 +0800

    Add data to overview

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,10 +1,13 @@
 <template>
 	<div class="container">
-		<Status class="status" />
+		<Status class="status"
+			:playerName="playerName"
+			:score="score" />
 	</div>
 </template>
 
 <script>
+// import data from '../data';
 import Status from '../components/Status.vue';
 
 export default {
@@ -12,8 +15,12 @@ export default {
 	components: {
 		Status,
 	},
+	created() {
+	},
 	data() {
 		return {
+			playerName: '',
+			score: '',
 			plantlist: [
 				{ name: 'Cucumber', health: 50, image: 'aseDf' },
 				{ name: 'Tomato', health: 35, image: 'dfWgF' },
@@ -24,7 +31,5 @@ export default {
 </script>
 
 <style scoped>
-	.status {
-		margin-top: 2%;
-	}
+
 </style>
