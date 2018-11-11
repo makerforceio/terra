3bb3ab11da7112099bd056362a099899cc924d04
Ambrose Chua <ambrose@makerforce.io>
2018-11-11 09:20:41 +0800

    Mappp

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,5 +1,6 @@
 <template>
 	<div class="map">
+		<div class="">
 		<div id="mapbox"></div>
 	</div>
 </template>
@@ -15,6 +16,106 @@ export default {
 			style: 'mapbox://styles/theplantgame/cjobk62y62bch2rs07qtfiaqs',
 			center: [103.7776456, 1.3033084],
 			zoom: 16.0,
+		});
+		for (let i = 1; i <= 5; i += 1) {
+			this.map.loadImage(`/users/user-${i}.jpg`, (err, image) => {
+				if (err) throw err;
+				this.map.addImage(`user-${i}`, image);
+			});
+		}
+		this.map.on('load', () => {
+			this.map.addLayer({
+				id: 'points',
+				type: 'symbol',
+				source: {
+					type: 'geojson',
+					data: {
+						type: 'FeatureCollection',
+						features: [
+							{
+								type: 'Feature',
+								properties: {
+									description: 'You',
+									icon: 'user-1',
+								},
+								geometry: {
+									type: 'Point',
+									coordinates: [
+										103.7779712677002,
+										1.3031515342080502,
+									],
+								},
+							},
+							{
+								type: 'Feature',
+								properties: {
+									description: 'Alison',
+									icon: 'user-2',
+								},
+								geometry: {
+									type: 'Point',
+									coordinates: [
+										103.77654433250426,
+										1.3039452626082444,
+									],
+								},
+							},
+							{
+								type: 'Feature',
+								properties: {
+									description: 'Solderneer',
+									icon: 'user-3',
+								},
+								geometry: {
+									type: 'Point',
+									coordinates: [
+										103.77538561820984,
+										1.3042992224897965,
+									],
+								},
+							},
+							{
+								type: 'Feature',
+								properties: {
+									description: 'Ambrose Chua',
+									icon: 'user-4',
+								},
+								geometry: {
+									type: 'Point',
+									coordinates: [
+										103.7764048576355,
+										1.3053074715764539,
+									],
+								},
+							},
+							{
+								type: 'Feature',
+								properties: {
+									description: 'Ambrose Chua',
+									icon: 'user-5',
+								},
+								geometry: {
+									type: 'Point',
+									coordinates: [
+										103.77560019493103,
+										1.3063586244491103,
+									],
+								},
+							},
+						],
+					},
+
+				},
+				layout: {
+					'icon-image': '{icon}',
+					'icon-size': 0.2,
+
+					'text-field': '{title}',
+					'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+					'text-offset': [0, 0.6],
+					'text-anchor': 'top',
+				},
+			});
 		});
 	},
 	beforeDestroy() {
